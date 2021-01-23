b63507a7afdbe472e95cec80fc2d55fb9a95c569
shahithyan <shahithyan2010@gmail.com>
2021-01-23 12:40:05 -0600

    if statement

---
+++ condstmt/add.js
@@ -0,0 +1,13 @@
+var number1 = 1;
+var number2 = 0;
+var sum = number1 + number2;
+
+console.log("Answer = " + sum);
+
+if(sum > 1){
+    console.log("Answer is greater than 1");
+} else if(sum === 1){
+    console.log("Answer is equal to 1")
+} else {
+    console.log("Answer is less than 1")
+}   

e1b52ce7763997188724aa2607ed16c7b8439811
shahithyan <shahithyan2010@gmail.com>
2021-01-24 19:33:44 -0600

    for loop

---
+++ loop/for.js
@@ -0,0 +1,9 @@
+
+var num = 9;
+console.log("before incrementing num = " + num);
+num++;
+console.log("after incrementing num = " + num);
+
+for(var i = 0; i < 10; i++){
+    console.log("Shahithyan and Darshan");
+}

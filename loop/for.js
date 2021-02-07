9f5b2a807c28fca74986fcc5e7594722af320d9e
shahithyan <shahithyan2010@gmail.com>
2021-02-07 11:32:51 -0600

    numbers

--- loop/for.js
+++ loop/for.js
@@ -3,7 +3,19 @@ var num = 9;
 console.log("before incrementing num = " + num);
 num++;
 console.log("after incrementing num = " + num);
+num--;
+console.log("after decrementing num = " + num);
 
-for(var i = 0; i < 10; i++){
+num = num + 5;
+console.log("Adding 5 to the number = " + num);
+num = num - 5;
+
+if(num > 5){
+    console.log("Number is greater than 5");
+}else{
+    console.log("Number is less than 5")
+}
+
+for(var i = 0; i < 2; i++){
     console.log("Shahithyan and Darshan");
 }

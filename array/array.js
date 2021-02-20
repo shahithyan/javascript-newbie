04f8ad71a8563925c46c130aa2680e71fa6b22cc
shahithyan <shahithyan2010@gmail.com>
2021-02-20 12:52:21 -0600

    learning array

---
+++ array/array.js
@@ -0,0 +1,18 @@
+var mathScore = 100;
+var englishScore = 100;
+
+var marks = [94, 95, 96, 97, 98, 99, 100];
+
+console.log("size of the Array is " + marks.length);
+
+/*console.log("the first element in the array is " + marks[0]);
+console.log("the next element in the array is " + marks[1]);
+console.log("the next element in the array is " + marks[2]);
+console.log("the next element in the array is " + marks[3]);
+console.log("the next element in the array is " + marks[4]);
+console.log("the next element in the array is " + marks[5]);
+console.log("the next element in the array is " + marks[6]);*/
+
+for(var i = 0; i < marks.length; i++){
+    console.log("value is " + marks[i])
+}

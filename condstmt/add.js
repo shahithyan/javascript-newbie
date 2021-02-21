826eadfa82ed3c49eb488cca9e17979c1f102c97
shahithyan <shahithyan2010@gmail.com>
2021-02-21 13:06:23 -0600

    learnind const and doing reminder

--- condstmt/add.js
+++ condstmt/add.js
@@ -1,5 +1,14 @@
 var number1 = 1;
 var number2 = 0;
+const number3 = 2;
+//number3 = 100;
+
+var number4 = 10;
+console.log("number4 is " + number4);
+number4 = 50;
+console.log("number4 is now" + number4)
+var reminder = 11 % 3
+console.log( "reminder is" + reminder)
 var sum = number1 + number2;
 
 console.log("Answer = " + sum);

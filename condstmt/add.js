7e295e0c52b1c5dd748d972c895dd334ccc02552
shahithyan <shahithyan2010@gmail.com>
2021-02-28 13:03:51 -0600

    Working with var and str

--- condstmt/add.js
+++ condstmt/add.js
@@ -6,9 +6,9 @@ const number3 = 2;
 var number4 = 10;
 console.log("number4 is " + number4);
 number4 = 50;
-console.log("number4 is now" + number4)
-var reminder = 11 % 3
-console.log( "reminder is" + reminder)
+console.log("number4 is now" + number4);
+var reminder = 11 % 3;
+console.log( "reminder is" + reminder);
 var sum = number1 + number2;
 
 console.log("Answer = " + sum);
@@ -16,7 +16,16 @@ console.log("Answer = " + sum);
 if(sum > 1){
     console.log("Answer is greater than 1");
 } else if(sum === 1){
-    console.log("Answer is equal to 1")
+    console.log("Answer is equal to 1");
 } else {
-    console.log("Answer is less than 1")
+    console.log("Answer is less than 1");
 }   
+    console.log('\'Shahithyan\' \'Darshan\'');
+    var str = "First " + "second";
+    console.log(str);
+    var myStr = "I am a good boy. ";
+    myStr += "My brother is a good boy.";
+    console.log(myStr);
+    var name = "Darshan.";
+    var nameStr = "My brother is " + name + " he is well";
+    console.log(nameStr);

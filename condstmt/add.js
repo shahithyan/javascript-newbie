dbfb9d9623b67c670d3105356f32de782657bdb8
shahithyan <shahithyan2010@gmail.com>
2021-03-06 15:09:42 -0600

    Str operation

--- condstmt/add.js
+++ condstmt/add.js
@@ -23,9 +23,27 @@ if(sum > 1){
     console.log('\'Shahithyan\' \'Darshan\'');
     var str = "First " + "second";
     console.log(str);
+
     var myStr = "I am a good boy. ";
     myStr += "My brother is a good boy.";
     console.log(myStr);
+
     var name = "Darshan.";
     var nameStr = "My brother is " + name + " he is well";
     console.log(nameStr);
+
+    var nameLength = 0;
+    var firstName = "Shahithyan";
+    nameLength = firstName.length;
+    console.log( "I have " + nameLength + "letters in my name");
+
+
+    var First = ""
+    var firstLetter = "Darshan";
+    firstLetter = firstLetter[0];
+    console.log("The first letter of my brother's name is " + firstLetter);
+
+
+    var cName = "Dan";
+    var lastLetter = cName[cName.length - 1];
+    console.log("The last letter in Dan is " + lastLetter);

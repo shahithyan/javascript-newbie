d1cc1f0068755537c0b462a3907d99462eb4abef
shahithyan <shahithyan2010@gmail.com>
2021-03-07 15:26:21 -0600

    printing words in the str

---
+++ loop/string.js
@@ -0,0 +1,12 @@
+var myName = "Shahithyan";
+
+for (var i = 0; i < myName.length; i++){
+    console.log(myName[i]);
+}
+
+console.log("Reverse");
+//This is going in reverse
+
+for(var j = myName.length - 1; j >= 0; j-- ) {
+    console.log(myName[j]);
+}

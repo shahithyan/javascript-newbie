d3bd1467959e7ca8c50592b24b49d4afd3718ed5
shahithyan <shahithyan2010@gmail.com>
2021-03-20 11:40:33 -0500

    learning function

---
+++ function/function.js
@@ -0,0 +1,24 @@
+function printNames() {
+    console.log("Shahithyan and darshan");
+}
+
+printNames();
+printNames();
+printNames();
+
+
+function add(num1, num2, num3) {
+    var sum = num1 + num2 + num3;
+    return sum;
+}
+
+function addTwoNumbers(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+ var answer = add(10, 10, 10);
+ console.log("The answer is " + answer);
+
+ const ans = addTwoNumbers(10, 10);
+ console.log("the answer is " + ans);
--- loop/string.js
+++ loop/string.js
@@ -10,3 +10,15 @@ console.log("Reverse");
 for(var j = myName.length - 1; j >= 0; j-- ) {
     console.log(myName[j]);
 }
+
+console.log("first five letters.");
+
+for (var i = 0; i < 5; i++) {
+    console.log(myName[i]);
+}
+
+console.log("last 5");
+
+for (var j = 5; j < 10; j++) {
+    console.log(myName[j]);
+}
